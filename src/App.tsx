9d40c30277a17005d983d0d065609bf0eb0d4161
RistoHaime <risto.haime@myy.haaga-helia.fi>
2025-04-10 13:57:51 +0300

    Footer laitettu App.tsx kansioon, jolloin se on kaikissa ikkunoissa

--- src/App.tsx
+++ src/App.tsx
@@ -35,6 +35,11 @@ function App() {
                   </Toolbar>
               </AppBar>
               <Outlet />
+              <footer style={{ marginTop: '390px', padding: '20px', backgroundColor: '#f1f1f1', textAlign: 'center', opacity: 0.9 }}>
+                <p>Owners: Omppu ja rane</p>
+                <p>Founded: 2025</p>
+                <p>Y-tunnus: 28hm45</p>
+            </footer>
          </Container>
         
       </QueryClientProvider>
