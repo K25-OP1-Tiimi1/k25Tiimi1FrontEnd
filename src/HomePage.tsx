57d3a16cd0874abd0f70c38680b7fc314444b96f
denwell76 <danielnieminen04@gmail.com>
2025-04-14 16:55:41 +0300

    duplicate footer poistettu

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -4,10 +4,9 @@ export default function HomePage(){
 
     //todo lisää toiminnallisuuksia
     return (
-        <div className="HomePage"  style={{height:500, width:"100%"}}>
+        <div className="HomePage"  style={{height:1000}}>
             HELLO!
-
-
         </div>
-    )
+    );
 }
+
