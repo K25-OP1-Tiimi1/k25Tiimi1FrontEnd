979f7862d84dc4afd33bd1e173687137da60fe31
RistoHaime <risto.haime@myy.haaga-helia.fi>
2025-04-14 21:11:57 +0300

    products sivu toimii back endin kanssa

--- src/api.jsx
+++ src/api.jsx
@@ -1,6 +1,7 @@
 const fetchProducts = async () => {
-    const response = await fetch( "http://localhost:8080/products");
+    const response = await fetch("http://localhost:8080/products")
     const data = await response.json();
     return data;
+ 
 }
 export{fetchProducts};
