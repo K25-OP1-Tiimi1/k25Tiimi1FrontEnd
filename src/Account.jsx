0202c8c771dcbdf0f9be097f3ff7562237b42664
jussilampinen <bhi090@myy.haaga-helia.fi>
2025-04-22 14:56:58 +0300

    API-reititys korjattu

--- src/Account.jsx
+++ src/Account.jsx
@@ -1,11 +1,11 @@
-import {  Dialog, DialogActions, DialogContent, DialogTitle, TextField } from "@mui/material";
-import {  useState } from "react"
+import { Dialog, DialogActions, DialogContent, DialogTitle, TextField } from "@mui/material";
+import { useState } from "react"
 
 export default function Account() {
 
-    const [signUser, setSignUser] = useState({username:'', email:''}); 
+    const [signUser, setSignUser] = useState({ username: '', email: '' });
     const [user, setUser] = useState([])
-    const [registerUser, setRegisterUser] = useState({username:'', email:''})
+    const [registerUser, setRegisterUser] = useState({ username: '', email: '' })
 
     const [open, setOpen] = useState(false)
     const [openRegistForm, setOpenRegistForm] = useState(false)
@@ -13,134 +13,134 @@ export default function Account() {
 
     const handleClose = () => {
         setOpen(false),
-        setOpenRegistForm(false)
+            setOpenRegistForm(false)
     }
 
     const handleChange = event => {
-        setSignUser({...signUser,[event.target.name]: event.target.value})
+        setSignUser({ ...signUser, [event.target.name]: event.target.value })
     }
 
-    const handleRegisterChange =  event => {
-        setRegisterUser({...registerUser,[event.target.name]: event.target.value})
+    const handleRegisterChange = event => {
+        setRegisterUser({ ...registerUser, [event.target.name]: event.target.value })
     }
 
-    const handleSave =  async () => {
+    const handleSave = async () => {
         const options = {
-            method:'POST',
+            method: 'POST',
             headers: {
                 'content-Type': 'application/json',
             },
             body: JSON.stringify(registerUser)
         }
         setOpenRegistForm(false)
-        const response = await fetch("http://localhost:8080/registerUser", options);
+        const response = await fetch("http://localhost:8080/api/registerUser", options);
         const data = await response.json();
         console.log("registered", data)
-        
+
         return data;
     }
     const fetchUserByName = async (user) => {
-            fetch('http://localhost:8080/user/'+ user)
-            .then(response  => response.json())
+        fetch('http://localhost:8080/api/user/' + user)
+            .then(response => response.json())
             .then(data => {
                 setUser(data)
                 console.log(data)
             })
         setOpen(false);
         if (user != "" && user != null) {
-           setShowRegisterButton(false)
+            setShowRegisterButton(false)
         }
 
     }
 
     const deldeteCurrentUser = async () => {
-            const options ={
-              method: 'DELETE'
-            }
-              if(window.confirm("do you want to delete this car")){
-                  return fetch("http://localhost:8080/user/"+user?.id, options);
-              }
-         };  
-    
-        return (
-           <>  
+        const options = {
+            method: 'DELETE'
+        }
+        if (window.confirm("do you want to delete this car")) {
+            return fetch("http://localhost:8080/api/user/" + user?.id, options);
+        }
+    };
+
+    return (
+        <>
             <div
-            style={{float:"inline-end", marginLeft: 30}}>
-           
-            {showRegisterButton && <button 
-            style={{color:"black",background:"lightblue",fontSize:18, fontWeight:"bold", }}
-            onClick={() => setOpenRegistForm(true)}> Register </button>}
-            
-            <button 
-            style={{color:"black",background:"lightblue",fontSize:18, fontWeight:"bold" }}
-            onClick={() => setOpen(true)}> Sign In </button>
-            
+                style={{ float: "inline-end", marginLeft: 30 }}>
+
+                {showRegisterButton && <button
+                    style={{ color: "black", background: "lightblue", fontSize: 18, fontWeight: "bold", }}
+                    onClick={() => setOpenRegistForm(true)}> Register </button>}
+
+                <button
+                    style={{ color: "black", background: "lightblue", fontSize: 18, fontWeight: "bold" }}
+                    onClick={() => setOpen(true)}> Sign In </button>
+
             </div>
 
-            <h1 style={{background:"white"}}>
-            {user?.username}
-            <p>
-            {user?.email}
-            </p>
-            <button style={{fontSize:20}}
-            onClick={ () => deldeteCurrentUser()}>
-                Delete 
-            </button>
+            <h1 style={{ background: "white" }}>
+                {user?.username}
+                <p>
+                    {user?.email}
+                </p>
+                <button style={{ fontSize: 20 }}
+                    onClick={() => deldeteCurrentUser()}>
+                    Delete
+                </button>
             </h1>
-          
-           <Dialog 
-            open={open}
-            onClose={handleClose}
-            > 
+
+            <Dialog
+                open={open}
+                onClose={handleClose}
+            >
                 <DialogTitle>Sign In</DialogTitle>
-                <DialogContent> 
-                            <TextField 
-                            autoFocus
-                            required
-                            id="username"
-                            name="username"
-                            label="username"
-                            type="text"
-                            fullWidth
-                            onChange={handleChange}
-                            value={signUser.username}
-                            />
-                            <TextField 
-                            autoFocus
-                            required
-                            id="email"
-                            name="email"
-                            label="email"
-                            type="text"
-                            fullWidth
-                            onChange={handleChange}
-                            value={signUser.email}
-                            />
-                            </DialogContent>
+                <DialogContent>
+                    <TextField
+                        autoFocus
+                        required
+                        id="username"
+                        name="username"
+                        label="username"
+                        type="text"
+                        fullWidth
+                        onChange={handleChange}
+                        value={signUser.username}
+                    />
+                    <TextField
+                        autoFocus
+                        required
+                        id="email"
+                        name="email"
+                        label="email"
+                        type="text"
+                        fullWidth
+                        onChange={handleChange}
+                        value={signUser.email}
+                    />
+                </DialogContent>
                 <DialogActions>
-              <button onClick={() => handleClose()}>Cancel</button>
-              <button onClick={() => fetchUserByName(signUser.username)}>Sign in</button>
-            </DialogActions>
+                    <button onClick={() => handleClose()}>Cancel</button>
+                    <button onClick={() => fetchUserByName(signUser.username)}>Sign in</button>
+                </DialogActions>
             </Dialog>
 
-            <Dialog 
-            open={openRegistForm}
-            onClose={handleClose}
-            > 
+            <Dialog
+                open={openRegistForm}
+                onClose={handleClose}
+            >
                 <DialogTitle>Register</DialogTitle>
                 <DialogContent>
-            <TextField 
-            autoFocus
-            required
-            id="username"
-            name="username"
-            label="username"
-            type="text"
-            fullWidth
-            onChange={handleRegisterChange}
-            value={registerUser.username}
-            />
-            {/* <TextField 
+                    <TextField
+                        autoFocus
+                        required
+                        id="username"
+                        name="username"
+                        label="username"
+                        type="text"
+                        fullWidth
+                        onChange={handleRegisterChange}
+                        value={registerUser.username}
+                    />
+                    {/* <TextField 
             autoFocus
             required
             id="lastName"
@@ -151,23 +151,23 @@ export default function Account() {
             onChange={handleRegisterChange}
             value={registerUser.lastName}
             /> */}
-            <TextField 
-            autoFocus
-            required
-            id="email"
-            name="email"
-            label="email"
-            type="text"
-            fullWidth
-            onChange={handleRegisterChange}
-            value={registerUser.email}
-            />
-            </DialogContent>
+                    <TextField
+                        autoFocus
+                        required
+                        id="email"
+                        name="email"
+                        label="email"
+                        type="text"
+                        fullWidth
+                        onChange={handleRegisterChange}
+                        value={registerUser.email}
+                    />
+                </DialogContent>
                 <DialogActions>
-              <button onClick={() => {handleClose()}}>Cancel</button>
-              <button onClick={() => handleSave()}>Save</button>
-            </DialogActions>
+                    <button onClick={() => { handleClose() }}>Cancel</button>
+                    <button onClick={() => handleSave()}>Save</button>
+                </DialogActions>
             </Dialog>
-            </>
-        )
+        </>
+    )
 }
--- src/api.jsx
+++ src/api.jsx
@@ -1,12 +1,12 @@
 const fetchProducts = async () => {
-    const response = await fetch("http://localhost:8080/products")
+    const response = await fetch("http://localhost:8080/api/products")
     const data = await response.json();
     return data;
 
 }
 
 const fetchUsers = async () => {
-    const response = await fetch("http://localhost:8080/users");
+    const response = await fetch("http://localhost:8080/api/users");
     const data = await response.json();
     return data;
 
@@ -14,33 +14,33 @@ const fetchUsers = async () => {
 
 const registerUser = async (user) => {
     const options = {
-        method:'POST',
+        method: 'POST',
         headers: {
             'content-Type': 'application/json',
         },
         body: JSON.stringify(user)
     }
-    const response = await fetch("http://localhost:8080/registerUser", options);
+    const response = await fetch("http://localhost:8080/api/registerUser", options);
     const data = await response.json();
     console.log("registered", data)
     return data;
 }
 
 const fetchUserByUsername = async (username, email) => {
-    const response = await fetch('http://localhost:8080/user/'+ username);
+    const response = await fetch('http://localhost:8080/api/user/' + username);
     const data = await response.json();
-    console.log (data)
+    console.log(data)
     return data;
 
 };
 
 const deleteUserById = async (id) => {
-    const options ={
-      method: 'DELETE'
+    const options = {
+        method: 'DELETE'
     }
-      if(window.confirm("do you want to delete this car")){
-          return fetch("http://localhost:8080/user/"+id, options);
-      }
- };  
+    if (window.confirm("do you want to delete this car")) {
+        return fetch("http://localhost:8080/api/user/" + id, options);
+    }
+};
 
 export { fetchUsers, fetchProducts, registerUser, fetchUserByUsername, deleteUserById };
