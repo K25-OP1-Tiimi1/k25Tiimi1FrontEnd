bfe4e851fdd52eeb53ea0dd43f0ef3b82acdd8c6
RistoHaime <risto.haime@myy.haaga-helia.fi>
2025-04-30 06:31:07 +0300

    siistitty importteja

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.js'
+import App from './App'
 import { createHashRouter, RouterProvider } from 'react-router-dom'
-import ProductList from './ProductList.js'
-import HomePage from './HomePage.js'
+import ProductList from './ProductList'
+import HomePage from './HomePage'
 import AboutUs from './AboutUs'
-import UserList from './UserList.js'
-import Account from './Account.js'
+import UserList from './UserList'
+import Account from './Account'
 
 //routter
 const router = createHashRouter([
