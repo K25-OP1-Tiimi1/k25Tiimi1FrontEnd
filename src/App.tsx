61e142536b2d92ca6cb1fb117f3232f74e023009
JLampinan <jussitussi16@gmail.com>
2025-05-12 19:42:29 +0300

    Korjattu taustakuva importiksi.

--- src/App.tsx
+++ src/App.tsx
@@ -4,43 +4,42 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { AppBar, Container, CssBaseline, Toolbar, Typography } from '@mui/material';
 import { Link, Outlet } from 'react-router-dom';
 import FooterSection from './Footer.tsx'
-
-
+import backgroundImg from './assets/FreepikBackgroundImage.jpg';
 
 function App() {
-  
+
   const queryClient = new QueryClient();
 
-      return(
-     
-        <div className='backGroundImage' style={{backgroundImage: `url("src/assets/FreepikBackgroundImage.jpg")`,
-          
-         backgroundSize: 2400,
-         backgroundPosition:"center",
-        
-         }}>
-        <QueryClientProvider client={queryClient}>
-         <Container maxWidth="xl">
-          <h1 style = {{ backgroundColor: 'white' , fontFamily: 'serif'}} >Omppu & Rane</h1>
-              <CssBaseline />
-              <AppBar position="static" style={{opacity:0.9, height:80}} >
-                  <Toolbar variant='dense' >
-                      <Typography variant="h5">
-                        
-                        <nav>
-                          <Link to={"/"} style={{fontWeight:"bold", fontSize:30}}>Home Page</Link>
-                          <Link to={"/products"} style={{fontWeight:"bold", fontSize:30}}>Products</Link>
-                          <Link to={"/about-us"} style={{fontWeight:"bold", fontSize:30}}>About Us</Link>
-                          <Link to={"/account"} style={{fontWeight:"bold", fontSize:30}}>account</Link>
-                        </nav>
-                      </Typography>
-                  </Toolbar>
-              </AppBar>
-              <Outlet />
+  return (
+
+    <div className='backGroundImage' style={{
+      backgroundImage: `url(${backgroundImg})`,
+      backgroundSize: 2400,
+      backgroundPosition: "center",
+    }}>
+
+      <QueryClientProvider client={queryClient}>
+        <Container maxWidth="xl">
+          <h1 style={{ backgroundColor: 'white', fontFamily: 'serif' }} >Omppu & Rane</h1>
+          <CssBaseline />
+          <AppBar position="static" style={{ opacity: 0.9, height: 80 }} >
+            <Toolbar variant='dense' >
+              <Typography variant="h5">
+
+                <nav>
+                  <Link to={"/"} style={{ fontWeight: "bold", fontSize: 30 }}>Home Page</Link>
+                  <Link to={"/products"} style={{ fontWeight: "bold", fontSize: 30 }}>Products</Link>
+                  <Link to={"/about-us"} style={{ fontWeight: "bold", fontSize: 30 }}>About Us</Link>
+                  <Link to={"/account"} style={{ fontWeight: "bold", fontSize: 30 }}>account</Link>
+                </nav>
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          <Outlet />
           <FooterSection />
-         </Container>
+        </Container>
       </QueryClientProvider>
-      </div>
-      )
+    </div>
+  )
 }
 export default App
