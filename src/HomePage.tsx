daf4bdd1d040e9b377aef0e3475db9ca7f1d308a
RistoHaime <risto.haime@myy.haaga-helia.fi>
2025-05-13 17:05:34 +0300

    Lisätty ryhmäkuva, Vilma puuttuu

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,11 +1,11 @@
-import photo from './assets/FreepikBackgroundShadow.jpg';
+import photo from './assets/Team1PhotoEdit.jpg';
 export default function HomePage(){
 
 
     //todo lisää toiminnallisuuksia
     return (
         <p className="HomePage" style={{display:"flex", flexDirection:"column", alignItems: 'center', justifyContent: 'center',}}>    
-            <img src={photo} height={700} width={700} style={{border: '10px solid gray',}}/>
+            <img src={photo} height={700} width={1000} style={{border: '10px solid gray',}}/>
             <text   style={{backgroundColor:"white", fontSize:40, fontFamily:"fantasy"}}>DEV TEAM 1</text>
         </p>
     );
